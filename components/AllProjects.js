71b1502a8f9e992e61d6002f0f6b16bc1d5d479c
Horacio Gutierrez <hogutier@yahoo.com>
2018-05-07 15:26:36 -0500

    starting point

---
+++ components/AllProjects.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class AllProjects extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <div>
+        <h5> Welcome to AllProjects Page</h5>
+      </div>
+    )
+  }
+}
--- components/index.js
+++ components/index.js
@@ -3,19 +3,16 @@ import './index.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { HashRouter as Router } from 'react-router-dom'
 import {Provider} from 'react-redux'
 import store from './store'
+import Main from './Main'
 
 ReactDOM.render(
   <Provider store={store}>
-    <div id='demo' className='fill-xy bg-violet white column center-xy'>
-      <div>
-        <div>Actions&</div>
-        <div>Reducers&</div>
-        <div>Middleware&</div>
-        <div>Redux.</div>
-      </div>
-    </div>
+    <Router>
+      <Main />
+    </Router>
   </Provider>,
   document.getElementById('app')
 )
