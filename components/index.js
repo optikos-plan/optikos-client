494c3d1c49f841ecaaac14af372fe6850e6d1192
Amal Sudama <cds.sudama@gmail.com>
2018-05-14 15:31:09 -0400

    test env with webpack

--- components/index.js
+++ components/index.js
@@ -10,14 +10,15 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 import Main from './Main'
 
-// const GRAPHQL_URI = process.env.GRAPHQL_URI || 'http://localhost:3999/graphql'
-const GRAPHQL_URI = 'https://optikos-gql.herokuapp.com/graphql'
+const GRAPHQL_URI = process.env.GRAPHQL_URI || 'http://localhost:3999/graphql'
+// const GRAPHQL_URI = 'https://optikos-gql.herokuapp.com/graphql'
 
 const client = new ApolloClient({
   uri: GRAPHQL_URI
 })
-// console.log('GRAPHQL_URI', GRAPHQL_URI)
-// console.dir(process.env)
+
+console.log('GRAPHQL_URI', GRAPHQL_URI)
+console.dir(process.env)
 
 ReactDOM.render(
   <ApolloProvider client={client}>
