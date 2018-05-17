1940328a2b556d5731bfd8656ddd3dcc758b6347
Amal Sudama <cds.sudama@gmail.com>
2018-05-16 22:45:14 -0400

    deleteTask mutation shape fixed

--- components/task-diagram/mutations/createTask.js
+++ components/task-diagram/mutations/createTask.js
@@ -4,48 +4,49 @@ import { Mutation } from 'react-apollo'
 import RaisedButton from 'material-ui/RaisedButton'
 
 const mutationCreateTask = gql`
-  mutation createTask($projectId: ID!, $userId: ID!, $title: String! ) {
+  mutation createTask($projectId: ID!, $userId: ID!, $title: String!) {
     createTask(projectId: $projectId, userId: $userId, title: $title) {
-      id,
-      title,
-      status,
-      endDate,
+      id
+      title
+      status
+      endDate
       children {
         id
-      },
+      }
       user {
-        id,
+        id
         name
-      },
+      }
       project {
         id
       }
       parents {
         id
       }
+    }
   }
-}
 `
 
-const createTask = (props) => {
+const createTask = props => {
   return (
     <Mutation mutation={mutationCreateTask}>
-    {setTask => (
-      <RaisedButton label="ADD A TASK"  onClick={ async () => {
-      let { data } = await setTask({
-        variables: {
-          projectId: props.projectId,
-          userId: 1,
-          title: "Default Task"
-        }
-      })
-      const task = data.createTask
+      {setTask => (
+        <RaisedButton
+          label="ADD A TASK"
+          onClick={async () => {
+            let { data } = await setTask({
+              variables: {
+                projectId: props.projectId,
+                userId: 1,
+                title: 'Default Task'
+              }
+            })
+            const task = data.createTask
 
-      props.createTask(task)
-
-
-    } } />
-    )}
+            props.createTask(task)
+          }}
+        />
+      )}
     </Mutation>
   )
 }
