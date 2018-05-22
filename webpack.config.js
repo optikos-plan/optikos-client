6cae7af19a55df2782ab3aebe9ad71fdb2777277
Jason Y <jasonjyang1@gmail.com>
2018-05-22 15:48:26 -0400

    url fix

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
     new webpack.DefinePlugin({
       GRAPHQL_URI:
         process.env.NODE_ENV === 'production'
-          ? JSON.stringify('https://afternoon-mountain-72827.herokuapp.com//graphql')
+          ? JSON.stringify('https://afternoon-mountain-72827.herokuapp.com/graphql')
           : JSON.stringify('http://localhost:3999/graphql')
     })
   ]
